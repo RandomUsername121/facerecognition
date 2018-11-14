c2f04b63e021c66d9b90cda6b88eaf976b7a38ff
Tariq <tariq.berrada@student-cs.fr>
2018-11-14 10:41:27 +0100

    function1

--- __init__.py
+++ __init__.py
@@ -1,8 +1,21 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 path0 = "C:\\Users\\Samsung\\facerecognition\\Data\\Tetris_blocks.png"
 def load_and_display_image(path):
     img = cv2.imread(path, 1)
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-load_and_display_image(path0)
+#load_and_display_image(path0)
+
+def edges(filepath):
+    img = cv2.imread(filepath,1)
+    edges = cv2.Canny(img,100,200)
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+edges(path0)
