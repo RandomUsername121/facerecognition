0e4d222032fcd4e0f630bda240bdd5c90763970b
Erwan Dessailly <erwan.dessailly@student-cs.fr>
2018-11-14 15:14:07 +0100

    renomé les fichier

--- detection_visage.py
+++ detection_visage.py
@@ -29,4 +29,24 @@ def reco_webcam():
            break  # esc to quit
    cv.destroyAllWindows()
 
-reco_webcam()
+#reco_webcam()
+
+
+def detectionimage ():
+    face_cascade = cv.CascadeClassifier('C:/Users/Proprietaire/facerecognition/haarcascade/haarcascade_frontalface_default.xml')
+    eye_cascade = cv.CascadeClassifier('C:/Users/Proprietaire/facerecognition/haarcascade/haarcascade_eye.xml')
+    img = cv.imread('C:/Users/Proprietaire/facerecognition/data/antoinecautru (1).jpg')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv.imshow('img',img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+detectionimage()
