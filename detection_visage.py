00f6f681188b76353fe1ddd5d3e70b899d9838a5
Erwan Dessailly <erwan.dessailly@student-cs.fr>
2018-11-14 15:07:21 +0100

    renomé les fichier

--- detection_visage.py
+++ detection_visage.py
@@ -1,25 +1,32 @@
-
+# -*- coding: utf-8 -*
 import numpy as np
 import cv2 as cv
 
-path0 = "Data\\erwan2sal(1).jpg"
+def reco_webcam():
 
-def detection_visage(path):
-    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-    img = cv.imread(path)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+   face_cascade = cv.CascadeClassifier('C:/Users/Proprietaire/facerecognition/haarcascade/haarcascade_frontalface_default.xml')
+   eye_cascade = cv.CascadeClassifier('C:/Users/Proprietaire/facerecognition/haarcascade/haarcascade_eye.xml')
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    cv.imshow('img',img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+   #Webcam capture
+   video_capture = cv.VideoCapture(0)
+   while True :
+       ret, frame = video_capture.read()
+       frame = cv.flip(frame, 1)
 
-detection_visage(path0)
+       gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+       faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+
+       for (x,y,w,h) in faces:
+           cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+           roi_gray = gray[y:y+h, x:x+w]
+           roi_color = frame[y:y+h, x:x+w]
+           eyes = eye_cascade.detectMultiScale(roi_gray)
+           for (ex,ey,ew,eh) in eyes:
+               cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+       cv.imshow('img',frame)
+       if cv.waitKey(1) == 27:
+           break  # esc to quit
+   cv.destroyAllWindows()
+
+reco_webcam()
